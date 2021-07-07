a2430e2d723ae1652e770b3bb87952bd6cfebe8f
cyx233 <1070405216@qq.com>
2021-07-07 03:21:28 +0000

    process finish0707

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,7 @@
-import queue
+import time
 import struct
 import torch
+from torchvision.transforms.transforms import ToTensor
 from tqdm import tqdm
 from cogdata import DataManager
 import zipfile
@@ -10,7 +11,7 @@ import os
 from multiprocessing import Manager, Pool
 from torchvision import transforms
 from PIL import Image
-os.environ["CUDA_VISIBLE_DEVICES"] = "5"
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
 
 def process(input, output):
@@ -39,50 +40,52 @@ def process(input, output):
             file_list = [info for info in zip.infolist(
             ) if info.filename[-1] != os.sep]
             tot_len = len(file_list)
+            # tot_len = 100000
             transform = transforms.Compose([
                 transforms.Resize(img_size),
                 transforms.CenterCrop(img_size),
                 transforms.Normalize([0.79093, 0.76271, 0.75340], [0.30379, 0.32279, 0.32800])])
-            batch_size = buffer_size//4096*4
+            batch_size = buffer_size//(4096*4)
             mylen = (tot_len+read_k)//read_k
             begin_idx = list(range(0, tot_len, mylen))[id]
-            print_info = -10000
+            cnt = 0
             imgs = []
             txts = []
             for i in range(begin_idx, begin_idx+mylen):
-                # if len(txts) > batch_size:
-                #     lock.acquire()
-                #     index = input['index'].value
-                #     input["sum"].value += len(txts)
-                #     info.append(input['sum'].value)
-                #     input["index"].value += 1
-                #     lock.release()
-                #     imgs = torch.stack(imgs).cuda()
-                #     codes = img2code(model, imgs).cpu().numpy()
-                #     codes_byte = struct.pack(
-                #         len(codes[0])*"i", *(codes[0].tolist()))
-                #     for i in codes[1:]:
-                #         codes_byte += struct.pack(
-                #             len(i)*"i", *(i.tolist()))
-                #     with open(os.path.join(output_dir, str(index)+".txt"), "wb") as f:
-                #         f.write(str.join("\n", txts).encode("utf-8"))
-                #     with open(os.path.join(output_dir, str(index)+".bin"), "wb") as f:
-                #         f.write(codes_byte)
-                #     txts = []
-                #     imgs = []
+                if len(txts) >= batch_size:
+                    lock.acquire()
+                    index = input['index'].value
+                    input["sum"].value += len(txts)
+                    info.append(input['sum'].value)
+                    input["index"].value += 1
+                    lock.release()
+                    imgs = torch.stack(imgs)
+                    codes = img2code(model, imgs).cpu().tolist()
+                    codes_byte = struct.pack(
+                        len(codes[0])*"i", *codes[0])
+                    for j in codes[1:]:
+                        codes_byte += struct.pack(
+                            len(j)*"i", *j)
+                    with open(os.path.join(output_dir, str(index)+".txt"), "wb") as f:
+                        f.write(str.join("\n", txts).encode("utf-8"))
+                    with open(os.path.join(output_dir, str(index)+".bin"), "wb") as f:
+                        f.write(codes_byte)
+                    txts = []
+                    imgs = []
                 if i > tot_len:
                     break
-                # imgs.append(transform(transforms.ToTensor()(
-                #     Image.open(zip.open(file_list[i]))).cuda()))
-                img = Image.open(zip.open(file_list[i]))
-                img = transforms.functional.pil_to_tensor(img).float().cuda()
+                torch.cuda.synchronize()
+                f = Image.open(zip.open(file_list[i]))
+                # with Image.open(zip.open(file_list[i])) as f:
+                img = transforms.functional.pil_to_tensor(f).float().cuda()
                 img = transform(img)
                 imgs.append(img)
+                torch.cuda.synchronize()
                 dirs, filename = os.path.split(file_list[i].filename)
                 txts.append(text_dict[filename.split(".")[0]])
-                if (i-begin_idx)-print_info >= 1000:
-                    print("pid{} finish:{}/{}".format(os.getpid(), i-begin_idx, mylen))
-                    print_info = i-begin_idx
+                if cnt % 1000 == 0:
+                    print("pid{} finish:{}/{}".format(os.getpid(), cnt, mylen))
+                cnt += 1
             zip.close()
             if len(txts) > 0:
                 lock.acquire()
@@ -142,7 +145,7 @@ if __name__ == "__main__":
     parser.add_argument("--encode_size", type=int, default=32)
     parser.add_argument("--device", type=int, default=0)
     parser.add_argument("--batch_size", type=int, default=32)
-    parser.add_argument("--page_size", type=int, default=1024*1024)
+    parser.add_argument("--buffer_size", type=int, default=1024*1024)
     parser.add_argument('--img_tokenizer_num_tokens', type=int, default=None)
     parser.add_argument('--workers',  type=int, default=1)
     args = parser.parse_args()
@@ -154,7 +157,7 @@ if __name__ == "__main__":
                         "txt_files": ["/dataset/fd5061f6/cogview/dingming0629/sq_gouhou_white_pict_title_word_256_fulltitle.tsv"], "txt_type": "tsv"})
     img_size = args.encode_size * 8
     batch_size = args.batch_size
-    page_size = args.page_size
+    buffer_size = args.buffer_size
     workers = args.workers
     name = manager.get_name_by_label("test")[0]
     mode = manager.info[name]['mode']
@@ -191,7 +194,7 @@ if __name__ == "__main__":
     for i in range(workers):
         input_queue.put(i)
 
-    input = {"queue": input_queue, "text_dict": text_dict, "args": args, "buffer_size": page_size,
+    input = {"queue": input_queue, "text_dict": text_dict, "args": args, "buffer_size": buffer_size,
              "img_size": img_size, "lock": read_lock, "mode": mode, "k": workers, "sum": sum, "index": index}
     output = {"info": info}
 
