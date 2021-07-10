54d8764a9314c0a6e5cb1755ecdbdf58743c7ff8
minkowski0125 <mingding@a100-v100-jupyter-slave-4.a100-v100-jupyter-slave.dev-biiaabcjhgb.svc.cluster.local>
2021-07-10 12:15:09 +0000

    add argument

---
+++ cogdata/arguments.py
@@ -0,0 +1,68 @@
+import argparse
+
+def add_task_args(parser):
+    parser.add_argument('--task_name', type=str, default=None, help='name of the handling task.')
+    parser.add_argument('--task', type=str, default=None, help='type of the handling task.')
+    parser.add_argument('--saver', type=str, default='binary', help='saver mode.')
+    parser.add_argument('--length_per_sample', type=int, default=1089, help='data length of one sample (Bytes).')
+    parser.add_argument('--dtype', type=str, default='int32', help='data type of samples.')
+    parser.add_argument('--split_num', type=int, default=1, help='number of split pieces for the merge result.')
+
+    return parser
+
+def add_dataset_args(parser):
+    parser.add_argument('--dataset', type=str, default=None, help='name of the handling dataset.')
+    parser.add_argument('--description', type=str, default='', help='description of the handling dataset.')
+    parser.add_argument('--data_files', type=list, default=[], help='file names of the handling dataset.')
+    parser.add_argument('--data_format', type=str, default=None, help='format of data files.')
+    parser.add_argument('--text_file', type=str, default=None, help='file name of the text.')
+    parser.add_argument('--text_format', type=str, default=None, help='format of the text file.')
+    
+    return parser
+
+def get_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--action', type=str, default=None, help='user action')
+    parser.add_argument('--base_dir', type=str, default=None, help='base directory.')
+    
+    parser = add_task_args(parser)
+    parser = add_dataset_args(parser)
+
+    args = parser.parse_args()
+    return args
+
+'''
+task level:
+    python cli.py 
+        --action add_task
+        --task []
+        --task_name []
+        --saver []
+        --length_per_sample []
+        --dtype []
+
+    python cli.py 
+        --action process
+        --task_name []
+        --dataset [] （没有就处理所有未处理的）
+    
+    python cli.py 
+        --action merge
+        --task_name []
+
+    python cli.py 
+        --action split
+        --task_name []
+        --split_num []
+    
+dataset level:
+    python cli.py 
+        --action add_data
+        --dataset []
+        --description []
+        --data_files []
+        --data_format []
+        --text_file []
+        --text_format []
+'''
--- cogdata/cli.py
+++ cogdata/cli.py
@@ -3,7 +3,40 @@
 import argparse
 
 from data_manager import DataManager
+from arguments import get_args
+
+def add_task(manager, args):
+    pass
+
+def add_data(manager, args):
+    pass
+
+def process(manager, args):
+    pass
+
+def merge(manager, args):
+    pass
+
+def split(manager, args):
+    pass
 
 if __name__ == '__main__':
     print('cli')
     # use argparse to finish cli
+    args = get_args()
+    base_dir = args.base_dir
+    action = args.action
+
+    manager = DataManager(base_dir)
+    if action == 'add_task':
+        add_task(manager, args)
+    elif action == 'add_data':
+        add_data(manager, args)
+    elif action == 'process':
+        process(manager, args)
+    elif action == 'merge':
+        merge(manager, args)
+    elif action == 'split':
+        split(manager, args)
+    else:
+        print("Please give a correct action.")
