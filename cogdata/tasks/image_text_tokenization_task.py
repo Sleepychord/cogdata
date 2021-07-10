fda04885a422d70dc649b1c8ecc69e79111d1227
Ming Ding <dm_thu@qq.com>
2021-07-10 07:16:19 +0000

    load img transform

--- cogdata/tasks/image_text_tokenization_task.py
+++ cogdata/tasks/image_text_tokenization_task.py
@@ -4,10 +4,27 @@ import os
 import sys
 import math
 import random
+from PIL import Image
 
 from .base_task import BaseTask
 
 class ImageTextTokenizationTask(BaseTask):
     '''handle tokenization
     '''
-    pass
+    def __init__(self) -> None:
+        raise NotImplementedError
+    
+    def get_transform_fn(self, transform=None):
+        '''
+        Args:
+            transform: a transform in torchvision, do not use ToTensor().
+        '''
+        def transform_fn(fp, full_filename, local_transform=transform):
+            '''file obj to (PIL.Image, filename w/o suffix)
+            '''
+            img = Image.open(fp)
+            dirs, filename = os.path.split(full_filename)
+            filename = filename.split('.')[0]
+            if local_transform is not None:
+                img = local_transform(img)
+        return transform_fn
