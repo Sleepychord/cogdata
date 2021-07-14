2214f4444fab90d63807cfb8180c20d2a7d9c2c3
cyx233 <zwd18@mails.tsinghua.edu.cn>
2021-07-14 08:29:43 +0000

    change to subparser

--- arguments.py
+++ arguments.py
@@ -1,43 +1,34 @@
 import argparse
 
-def add_task_args(parser):
-    parser.add_argument('--task_id', type=str, default=None, help='id of the handling task.')
-    parser.add_argument('--task_type', type=str, default=None, help='type of the handling task.')
-    parser.add_argument('--saver', type=str, default='binary', help='saver mode.')
-    parser.add_argument('--length_per_sample', type=int, default=1089, help='data length of one sample (Bytes).')
-    parser.add_argument('--img_length', type=int, default=1024, help='length of a tokenized image.')
-    parser.add_argument('--txt_length', type=int, default=64, help='length of text in one sample.')
-    parser.add_argument('--dtype', type=str, default='int32', help='data type of samples.')
-    parser.add_argument('--split_num', type=int, default=1, help='number of split pieces for the merge result.')
-    parser.add_argument('--display_num', type=int, default=0, help='number of samples to randomly display')
-
-    return parser
-
-def add_dataset_args(parser):
-    parser.add_argument('--dataset', type=str, default=None, help='name of the handling dataset.')
-    parser.add_argument('--description', type=str, default='', help='description of the handling dataset.')
-    parser.add_argument('--data_files', type=list, default=[], help='file names of the handling dataset.')
-    parser.add_argument('--data_format', type=str, default=None, help='format of data files.')
-    parser.add_argument('--text_file', type=str, default=None, help='file name of the text.')
-    parser.add_argument('--text_format', type=str, default=None, help='format of the text file.')
-    
-    return parser
-
-def add_processor_args(parser):
-    parser.add_argument('--local_rank', type=int, default=None, help='rank number for processor run_single')
-    parser.add_argument('--args_dict', type=str, default='{}')
-
-    return parser
-
 def get_args():
     parser = argparse.ArgumentParser()
-
     parser.add_argument('--action', type=str, default=None, help='user action')
     parser.add_argument('--base_dir', type=str, default=None, help='base directory.')
+
+    subparsers = parser.add_subparsers()
+
+    subparser = subparsers.add_parser("task")
+    subparser.add_argument('--task_id', type=str, default=None, help='id of the handling task.')
+    subparser.add_argument('--task_type', type=str, default=None, help='type of the handling task.')
+    subparser.add_argument('--saver', type=str, default='binary', help='saver mode.')
+    subparser.add_argument('--length_per_sample', type=int, default=1089, help='data length of one sample (Bytes).')
+    subparser.add_argument('--img_length', type=int, default=1024, help='length of a tokenized image.')
+    subparser.add_argument('--txt_length', type=int, default=64, help='length of text in one sample.')
+    subparser.add_argument('--dtype', type=str, default='int32', help='data type of samples.')
+    subparser.add_argument('--split_num', type=int, default=1, help='number of split pieces for the merge result.')
+    subparser.add_argument('--display_num', type=int, default=0, help='number of samples to randomly display')
+
+    subparser = subparsers.add_parser("dataset")
+    subparser.add_argument('--dataset', type=str, default=None, help='name of the handling dataset.')
+    subparser.add_argument('--description', type=str, default='', help='description of the handling dataset.')
+    subparser.add_argument('--data_files', type=list, default=[], help='file names of the handling dataset.')
+    subparser.add_argument('--data_format', type=str, default=None, help='format of data files.')
+    subparser.add_argument('--text_file', type=str, default=None, help='file name of the text.')
+    subparser.add_argument('--text_format', type=str, default=None, help='format of the text file.')
     
-    parser = add_task_args(parser)
-    parser = add_dataset_args(parser)
-    parser = add_processor_args(parser)
+    subparser = subparsers.add_parser("processor")
+    subparser.add_argument('--local_rank', type=int, default=None, help='rank number for processor run_single')
+    subparser.add_argument('--args_dict', type=str, default='{}')
 
     args = parser.parse_args()
     return args
