dd7bddaca8318bdd6ec3aa963d580a6d52f614f5
minkowski0125 <zwd18@mails.tsinghua.edu.cn>
2021-12-17 12:58:38 +0000

    add hierarchical vqvae module

---
+++ cogdata/utils/ice_tokenizer/vqvae_tokenizer/api.py
@@ -0,0 +1,105 @@
+import importlib
+import torch
+import json
+import math
+import os
+from datetime import datetime
+from tqdm import tqdm
+import time
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+from torch.utils.data import DataLoader
+from torchvision.utils import save_image, make_grid
+from torchvision import transforms
+import torch.nn.functional as F
+
+PRETRAINED_MODEL_FILE = ""
+
+def new_module(config):
+    '''in config:
+            "target": module type
+            "params": dict of params'''
+    if type(config) == str:
+        with open(config, 'r') as file:
+            config = json.load(file)
+    assert type(config) == dict
+    if not "target" in config:
+        raise KeyError("Expected key `target` to instantiate.")
+    module, cls = config.get('target').rsplit(".", 1)
+    model = getattr(importlib.import_module(module, package=None), cls)(**config.get("params", dict()))
+    
+    return model
+
+def load_ckpt(model, path=None):
+    if path is None:
+        path = PRETRAINED_MODEL_FILE
+    sd = torch.load(path, map_location="cpu")['module']
+    model.load_state_dict(sd, strict=False)
+    return model
+
+def load_default_hvqvae():
+    config = {
+        "target": "vqvae.DSHVQVAE",
+        "params": {
+            "levels": 3,
+            "embedding_dim": 256,
+            "codebook_scale": 1,
+            "down_sampler_configs": [
+                {
+                    "target": "vqvae.ResidualDownSample",
+                    "params": {
+                        "in_channels": 256
+                    }
+                },
+                {
+                    "target": "vqvae.ResidualDownSample",
+                    "params": {
+                        "in_channels": 256
+                    }
+                }
+            ],
+            "enc_config": {
+                    "target": "vqvae.Encoder",
+                    "params": {
+                        "num_res_blocks": 2,
+                        "channels_mult": [1,2,4]
+                    }
+            },
+            "quantize_config": {
+                    "target": "vqvae.VectorQuantizeEMA",
+                    "params": {
+                        "hidden_dim": 256,
+                        "embedding_dim": 256,
+                        "n_embed": 20000,
+                        "training_loc": false
+                    }
+            },
+            "dec_configs": [
+                {
+                    "target": "vqvae.Decoder",
+                    "params": {
+                        "channels_mult": [1,1,1,2,4]
+                    }
+                },
+                {
+                    "target": "vqvae.Decoder",
+                    "params": {
+                        "channels_mult": [1,1,2,4]
+                    }
+                },
+                {
+                    "target": "vqvae.Decoder",
+                    "params": {
+                        "channels_mult": [1,2,4]
+                    }
+                }
+            ]
+        }
+    }
+    return new_module(config)
+
+
+if __name__ == '__main__':
+    pass
---
+++ cogdata/utils/ice_tokenizer/vqvae_tokenizer/tokenizer.py
@@ -0,0 +1,83 @@
+import math
+import torch
+import torch.nn.functional as F
+
+from .api import load_default_hvqvae, load_ckpt
+
+class VQVAETokenizer(object):
+    def __init__(self,
+                    model_path=None,
+                    device='cuda'):
+        model = load_default_hvqvae()
+        model = load_ckpt(model, model_path)
+        model = model.to(device)
+        model.eval()
+
+        self.model = model
+        self.device = device
+        self.image_tokens = model.quantize.n_embed
+        self.num_tokens = model.quantize.n_embed
+
+    def __len__(self):
+        return self.num_tokens
+
+    def img2code(self, img, l=None):
+        '''Convert a batch of img to code
+        Args:
+            model: The tokenizer model.
+            img: [b, c, h, w]
+        '''
+        if l is None:
+            with torch.no_grad():
+                quants, diffs, ids = self.model.encode(img)
+            return [id.view(img.shape[0], -1) for id in ids]
+        else:
+            with torch.no_grad():
+                quant, diff, id = self.model.single_encode(img, l)
+            return id.view(img.shape[0], -1)
+
+    def code2img(self, codes, l=None):
+        '''Convert a batch of code to imgs
+        Args:
+            model: ...
+            codes w/o l: [l, b, h, w] or [l, b, h*w] List of LongTensor
+            codes w/ l: [b, h, w] or [b, h*w] LongTensor
+        '''
+        if l is None:
+            if len(codes[0].shape) == 2:
+                s = int(math.sqrt(len(codes[0].view(-1))) + 1e-5)
+                b = codes[0].shape[0]
+                codes = [code.view(b, s, s) for code in codes]
+            with torch.no_grad():
+                out = model.decode_code(codes)
+                out = [item * torch.tensor([0.30379, 0.32279, 0.32800], device=out.device).view(1, -1, 1, 1) + torch.tensor([0.79093, 0.76271, 0.75340], device=out.device).view(1, -1, 1, 1) for item in out]
+        else:
+            if len(codes.shape) == 2:
+                s = int(math.sqrt(len(codes.view(-1))) + 1e-5)
+                codes = codes.view(codes.shape[0], s, s)
+            with torch.no_grad():
+                out = model.single_decode_code(codes)
+                out = out * torch.tensor([0.30379, 0.32279, 0.32800], device=out.device).view(1, -1, 1, 1) + torch.tensor([0.79093, 0.76271, 0.75340], device=out.device).view(1, -1, 1, 1)
+        return out
+
+    def EncodeAsIds(self, img, l=None):
+        assert len(img.shape) == 4 # [b, c, h, w]
+        return self.img2code(img, l)
+    
+    def DecodeIds(self, codes, l=None):
+        return self.code2img(codes, l)
+
+    def read_img(self, path, img_size=256):
+        tr = transform.Compose([
+            transforms.Resize(img_size),
+            transforms.CenterCrop(img_size),
+            transforms.ToTensor(),
+        ])
+        img = tr(Image.open(path))
+        if img.shape[0] == 4:
+            img = img[:-1]
+        tr_normalize = transforms.Normalize([0.79093, 0.76271, 0.75340], [
+                                            0.30379, 0.32279, 0.32800])
+        img = tr_normalize(img)
+        img = img.unsqueeze(0).float().to(self.device) # size [1, 3, h, w]
+        return img
